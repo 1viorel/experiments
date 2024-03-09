00bf7e24717b9ed0029873730829db57c195a8fc
Viorel <soituviorel@gmail.com>
2024-03-09 14:47:30 +0200

    cards update

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeCardComponent } from '../../components/home-card/home-card.component';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,22 @@ import { HomeCardComponent } from '../../components/home-card/home-card.componen
   templateUrl: './home.component.html',
   styles: ``,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  cards: Card[] = [
+    {
+      title: 'Fancy Hover',
+      body: 'This looks cool on movie posters/video game icons.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Card 2',
+      body: 'This is the body of card 2',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      title: 'Card 3',
+      body: 'This is the body of card 3',
+      image: 'https://via.placeholder.com/150',
+    },
+  ];
+}
